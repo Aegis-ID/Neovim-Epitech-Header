23ae1d5fb11ff34ccf9c3382cb938af9c423ca17
Aegis-ID <pierre.nguyen@epitech.eu>
2025-02-09 06:58:16 +0100

    feat: add support for python and haskell

--- lua/header.lua
+++ lua/header.lua
@@ -2,16 +2,18 @@ local M = {}
 
 M.insert_epitech_header = function()
     local filename = vim.fn.expand("%:t")
+    local filetype = vim.bo.filetype
     local current_year = os.date("%Y")
     local description = vim.fn.input("File description: ")
-    local header = string.format([[
-/*
-** EPITECH PROJECT, %s
-** %s
-** File description: 
-** %s
-*/
-]], current_year, filename, description)
+    local comment_styles = {
+        c = {"/*", "**", "*/"},
+        cpp = {"/*", "**", "*/"},
+        haskell = {"{-", " -", "-}"},
+        python = {""""", "", """""},
+    }
+    local style = comment_styles[filetype] or {"/*", "**", "*/"}
+    local header = string.format("%s\n%s EPITECH PROJECT, %s\n%s %s\n%s File description:\n%s %s\n%s",
+        style[1], style[2], current_year, style[2], filename, style[2], style[2], description, style[3])
     vim.api.nvim_buf_set_lines(0, 0, 0, false, vim.split(header, "\n"))
 end
 
