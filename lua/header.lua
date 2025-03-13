b9742ff0c6b9e2db24c5a17a8f0cd50dc9a849b4
Aegis-ID <pierre.nguyen@epitech.eu>
2025-03-13 04:41:20 +0100

    feat: add preprocessor directives if header files

--- lua/header.lua
+++ lua/header.lua
@@ -17,10 +17,19 @@ M.insert_epitech_header = function()
     local style = comment_styles[filetype] or {"/*", "**", "*/"}
     local header = string.format("%s\n%s EPITECH PROJECT, %s\n%s %s\n%s File description:\n%s %s\n%s",
         style[1], style[2], current_year, style[2], filename, style[2], style[2], description, style[3])
+
+    local is_header = filename:match("%.h$") or filename:match("%.hpp$")
+
+    if is_header then
+        local guard_name = string.upper(filename_no_ext) .. "_H__"
+        header = header .. "\n\n#ifndef __" .. guard_name .. "\n#define __" .. guard_name .. "\n\n\n\n#endif /* !__" .. guard_name .. " */"
+    end
+
     vim.api.nvim_buf_set_lines(0, 0, 0, false, vim.split(header, "\n"))
 end
 
-function M.setup()
+M.setup = function(opts)
+    opts = opts or {}
     vim.api.nvim_set_keymap("n", "<Leader>h", ":lua require'header'.insert_epitech_header()<CR>", { noremap = true, silent = true })
 end
 
