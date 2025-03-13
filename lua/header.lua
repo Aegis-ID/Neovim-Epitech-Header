aea2c00b3ad827d73c5ee784fa7cea397dbff67e
Aegis-ID <pierre.nguyen@epitech.eu>
2025-03-13 04:49:41 +0100

    feat: preprocessor directives naming

--- lua/header.lua
+++ lua/header.lua
@@ -14,15 +14,22 @@ M.insert_epitech_header = function()
         asm = {";", ";;", ";"},
         makefile = {"##", "##", "##"},
     }
+
+    -- Setup the base header
     local style = comment_styles[filetype] or {"/*", "**", "*/"}
     local header = string.format("%s\n%s EPITECH PROJECT, %s\n%s %s\n%s File description:\n%s %s\n%s",
         style[1], style[2], current_year, style[2], filename, style[2], style[2], description, style[3])
 
+    -- Setup the Preprocessor directive
     local is_header = filename:match("%.h$") or filename:match("%.hpp$")
 
     if is_header then
-        local guard_name = string.upper(filename_no_ext) .. "_H__"
-        header = header .. "\n\n#ifndef __" .. guard_name .. "\n#define __" .. guard_name .. "\n\n\n\n#endif /* !__" .. guard_name .. " */"
+        local default_guard = "__" .. string.upper(filename_no_ext) .. "_H__"
+        local guard_name = vim.fn.input("Preprocessor directive name: ", default_guard)
+        if guard_name == "" then
+            guard_name = default_guard
+        end
+        header = header .. "\n\n#ifndef " .. guard_name .. "\n#define " .. guard_name .. "\n\n\n\n#endif /* !" .. guard_name .. " */"
     end
 
     vim.api.nvim_buf_set_lines(0, 0, 0, false, vim.split(header, "\n"))
